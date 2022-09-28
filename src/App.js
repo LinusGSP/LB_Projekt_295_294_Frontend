e66caf23a27f5944cd415d4ae6e487fe36cacdea
Linus <schoenbaechler.linus@gmail.com>
2022-09-28 18:38:09 +0200

    Removed Choose

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Layout from './Layout';
 import Home from './Home';
 import Answer from './Answer';
 import Cards from './Cards';
-import Choose from './Choose';
 import NotFound from './NotFound';
 import LearnSet from './LearnSet'
 import CreateLearnSet from './CreateLearnSet'
@@ -38,7 +37,6 @@ function App() {
         <Route path=':id' element={<LearnSet />}/>
         <Route path=':id/answer' element={<Answer />}/>
         <Route path=':id/cards' element={<Cards />}/>
-        <Route path=':id/choice' element={<Choose />}/>
 
         <Route path='notfound' element={<NotFound />}/>
         

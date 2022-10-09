e7bbca575280bf01812caf86d5df589479117440
Linus <schoenbaechler.linus@gmail.com>
2022-10-09 11:08:22 +0200

    styled cards

--- src/Cards.js
+++ src/Cards.js
@@ -1,22 +1,28 @@
 import { React, useState } from "react"
-import { useLocation } from "react-router";
+import { redirect, useLocation } from "react-router";
 
 
 export default function Cards(props) {
-    const location = useLocation();
-    const [index, setIndex] = useState(0)
+    const location = useLocation(); // get state
 
-    const words = location.state.words
-    const learnSet = location.state.learnSet
+    const [index, setIndex] = useState(0) // new state of the index of the current card
+    const [isClicked, setIsClicked] = useState(0) // state for which side of the card should be shown
 
+    const words = location.state.words  // get all the learnset words
+    const learnSet = location.state.learnSet // get the learnset info
+
+
+    // change the current card
     const minusHandler = () => { setIndex(index - 1) }
     const plusHandler = () => { setIndex(index + 1) }
 
+    // create new date from unix
     let creationDate = new Date(learnSet.creationDate)
     let lastEdited = new Date(learnSet.lastEdited)
 
+    // all learnset infos
     const LearnSetInfo = <div>
-    <h1>{learnSet.name} {learnSet.language1?.flag} ➜ {learnSet.language2?.flag}  </h1>
+    <h1 onClick={() => window.location.replace("http://localhost:3000/" + learnSet.id)}>{learnSet.name} {learnSet.language1?.flag} ➜ {learnSet.language2?.flag}  </h1>
     <div className="learnsetinfo">
             <div className="date">
                 <p><strong>Erstell datum:</strong> <br />{creationDate.getDate() + "/" + creationDate.getMonth()+ "/" + creationDate.getFullYear() }</p>
@@ -29,21 +35,33 @@ export default function Cards(props) {
         </div>
     </div>
 
+    // the main card
     const recordCard = 
-    <div className="recordCard">
-        <div>
-            <h1>{words[index].translation}</h1>
+    <div className="grid-align">
+        <div className="recordCard" onClick={() => setIsClicked(isClicked + 1)}>
+            <h1>{isClicked % 2 === 0 ? words[index].word : words[index].translation}</h1>
         </div>
     </div>
 
-    const buttons = <div className="grid-align">
-        {index === 0 ? null : <button onClick={minusHandler}>zurück</button>}
-        {index === words.length - 1 ? null : <button onClick={plusHandler}>weiter</button>}
+    // next and back button
+    const buttons = 
+        <div className="grid-align">
+            <div >
+            {index === 0 ? null : <button onClick={minusHandler} className="btn">zurück</button>}
+            {index === words.length - 1 ? null : <button onClick={plusHandler}  className="btn">weiter</button>}
+            </div>
         </div>
+
+    // show current index
+    const currentPos = <div className="grid-align">
+    {index + 1}/{words.length}
+    </div>
+
 
     return (
         <div className="content">
             {LearnSetInfo}
+            {currentPos}
             {recordCard}
             {buttons}
         </div>
